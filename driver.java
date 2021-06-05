c9187840df8c02e79f153d851bfed0b171b754a3
logdogs <ecohendo@u.rochester.edu>
2021-06-05 10:03:04 -0500

    Connected the backend to the keyboard GUI, it now parses and converts. However, there are two things that need fixing:     1) It still needs to replace the input syllable with the converted         one     2) It needs to be able to handle copy-paste input

--- driver.java
+++ driver.java
@@ -3,13 +3,13 @@ import java.io.UnsupportedEncodingException;
 public class driver {
 
     public static void main(String[] args) {
-        // keyboard k = new keyboard();
-        // k.run();
-        try {
-            System.setOut(new PrintStream(System.out, true, "UTF-8"));
-            System.out.printf("你");
-            tests tester = new tests();
-            tester.testConversions();
-        } catch(UnsupportedEncodingException e) { e.printStackTrace(); }
+        keyboard k = new keyboard();
+        k.run();
+        // try {
+        //     System.setOut(new PrintStream(System.out, true, "UTF-8"));
+        //     System.out.printf("你");
+        //     tests tester = new tests();
+        //     tester.testConversions();
+        // } catch(UnsupportedEncodingException e) { e.printStackTrace(); }
     }    
 }
--- keyboard.java
+++ keyboard.java
@@ -6,7 +6,8 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
 import javax.swing.GroupLayout;
-import javax.swing.SwingConstants;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
 
 public class keyboard extends JFrame implements ActionListener,KeyListener{
     
@@ -14,6 +15,9 @@ public class keyboard extends JFrame implements ActionListener,KeyListener{
     private JTextField text;
     private final String TITLE = "Pinyin Keyboard";
     private final String INSTRUCTIONS = "";
+    private backend back = new backend();
+    private boolean syllableFinished;
+    private String syllable;
 
     public keyboard() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -22,6 +26,9 @@ public class keyboard extends JFrame implements ActionListener,KeyListener{
         
         this.instructions = new JLabel(INSTRUCTIONS);
         this.text = new JTextField(10);
+        this.text.addKeyListener(this);
+        this.syllable = "";
+        this.syllableFinished = false;
 
         GroupLayout layout = new GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -43,21 +50,55 @@ public class keyboard extends JFrame implements ActionListener,KeyListener{
 
     }
 
+    // We want to detect when a number has been entered into the text field. Then, we know it's time to parse a syllable. However, we need to filter
+    //  out bad input. Think about things like typos of the form "n3i". That should be left as it, and we should resume parsing as normal after we see
+    //  either:
+    //      1) A space separating syllables
+    //      2) 
+
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
     }
     @Override
-    public void keyPressed(KeyEvent e) {
-
-    }
+    public void keyPressed(KeyEvent e) {}
     @Override
-    public void keyReleased(KeyEvent e) {
-
-    }
+    public void keyReleased(KeyEvent e) {}
     @Override
     public void keyTyped(KeyEvent e) {
-
+        // We want to handle the case where we detect a number or whitespace, then we parse what we've seen.
+        // so as soon as we hit either one of those, we know we've got a new syllable to parse.
+        // But for this to work, we need to keep track of the characters we've seen so far
+        switch(e.getKeyChar()) {
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+                // First we convert
+                this.syllable += e.getKeyChar();
+                String converted = back.convertSyllable(this.syllable);
+                try{
+                    System.setOut(new PrintStream(System.out, true, "UTF-8"));
+                    System.out.printf("%s --> %s\n", this.syllable, converted);
+                }catch (UnsupportedEncodingException err) { err.printStackTrace(); }
+                // Second we replace
+                String current = this.text.getText();
+                current = current.replace(this.syllable,converted);
+                this.text.setText(current);
+                this.syllable = "";
+                break;
+            case ' ':
+            case '\n':
+            case '\t':
+            case '\r':
+                // This is the case where we have the neutral tone, so no conversion is necessary,
+                //  thus we just leave it alone and more on to the next syllable.
+                this.syllable = "";
+                break;
+            default:
+                this.syllable += e.getKeyChar();
+        }
     }
 
     public void run() {
